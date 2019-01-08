d0dc2482840b53b2ee278fa182420e957e587392
Diptis Halder <diptis.halder@gmail.com>
2019-01-08 16:38:18 +0000

    Updated docking_station.rb and its spec file, trying to pass creation of Bike object test.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,29 @@ require 'docking_station'
 RSpec.describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 end
+
+RSpec.describe DockingStation do
+  context 'bike creation' do
+    it 'Object created' do
+      dock_stn = DockingStation.new
+      expect(dock_stn.release_bike).to eq true
+    end
+
+    it 'Bike good?' do
+      dock_stn = DockingStation.new
+      bike = dock_stn.release_bike
+      expect(bike.working?).to eq true
+    end
+  end
+end
+
+
+# require 'scrabble'
+
+# RSpec.describe Scrabble do
+#   context 'standard specs' do
+#     it 'returns 0 for empty words' do
+#       scrabble = Scrabble.new('')
+
+#       expect(scrabble.score).to eq 0
+#     end
