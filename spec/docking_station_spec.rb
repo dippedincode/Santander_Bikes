87b80731c44a5b5146a2c51808c6a73c769e6b06
Diptis Halder <diptis.halder@gmail.com>
2019-01-08 18:47:21 +0000

    removed commented out parts from docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,29 +11,3 @@ end
 RSpec.describe DockingStation.new.release_bike.working? do
     it { should be true }
 end
-
-# RSpec.describe DockingStation do
-#   context 'bike creation' do
-#     it 'Object created' do
-#       dock_stn = DockingStation.new
-#       expect(dock_stn.release_bike).to eq
-#     end
-
-#     it 'Bike good?' do
-#       dock_stn = DockingStation.new
-#       bike = dock_stn.release_bike
-#       expect(bike.working?).to eq true
-#     end
-# end
-
-
-
-# require 'scrabble'
-
-# RSpec.describe Scrabble do
-#   context 'standard specs' do
-#     it 'returns 0 for empty words' do
-#       scrabble = Scrabble.new('')
-
-#       expect(scrabble.score).to eq 0
-#     end
