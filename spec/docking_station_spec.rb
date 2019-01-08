3199927707f8773282eabb6fc0c2589164477e69
Diptis Halder <diptis.halder@gmail.com>
2019-01-08 16:53:17 +0000

    Updated bike.rb and docking_station_spec.rb for test to get a bike, trying for working bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,20 +4,28 @@ RSpec.describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 end
 
-RSpec.describe DockingStation do
-  context 'bike creation' do
-    it 'Object created' do
-      dock_stn = DockingStation.new
-      expect(dock_stn.release_bike).to eq true
+RSpec.describe DockingStation.new.release_bike do
+  it { should be }
     end
 
-    it 'Bike good?' do
-      dock_stn = DockingStation.new
-      bike = dock_stn.release_bike
-      expect(bike.working?).to eq true
-    end
-  end
-end
+RSpec.describe DockingStation.new.release_bike.working? do
+      it { should be_true }
+        end
+
+# RSpec.describe DockingStation do
+#   context 'bike creation' do
+#     it 'Object created' do
+#       dock_stn = DockingStation.new
+#       expect(dock_stn.release_bike).to eq
+#     end
+
+#     it 'Bike good?' do
+#       dock_stn = DockingStation.new
+#       bike = dock_stn.release_bike
+#       expect(bike.working?).to eq true
+#     end
+# end
+
 
 
 # require 'scrabble'
