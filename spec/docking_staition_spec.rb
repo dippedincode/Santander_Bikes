306b4bbedc3dabc41c7bb3520592aff8b65b97bf
Debora <debora.piu92@gmail.com>
2019-01-09 16:06:32 +0000

    new method working? for Bike and related tests

--- spec/docking_staition_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'docking_station.rb'
-
-describe DockingStation do
-it { is_expected.to respond_to(:release_bike)}
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,8 @@
+require 'docking_station'
+require 'bike'
+
+describe DockingStation do
+  it { is_expected.to respond_to(:release_bike)}
+  it { expect(subject.release_bike).to be_kind_of(Bike)}
+
+end
